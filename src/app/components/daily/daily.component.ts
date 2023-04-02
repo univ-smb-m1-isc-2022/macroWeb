c2a04dc31187b7e66c5083885cbec7addc993858
cheddar <irilind.salihi@gmail.com>
2023-04-02 14:57:54 +0200

    [ + ] Updated component Can add menu delete elements from menu etc... Still some error to fix and need to add css

---
+++ src/app/components/daily/daily.component.ts
@@ -0,0 +1,202 @@
+import {ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import {MatTable} from "@angular/material/table";
+import {MenuServiceService} from "../../services/menu-service.service";
+import {MatDialog} from "@angular/material/dialog";
+import {menuChoiceComponent} from "./menuDialog/menuChoice.component";
+import {AddFoodComponent} from "./foodDialog/addFood.component";
+
+
+@Component({
+  selector: 'app-daily',
+  templateUrl: './daily.component.html',
+  styleUrls: ['./daily.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
+})
+export class DailyComponent implements OnInit{
+
+  @ViewChild('table', { static: true }) table!: MatTable<any>;
+  @ViewChild('menuNameInput', { static: true }) menuNameInput!: ElementRef;
+  menuIdList: any = [];
+
+  nutrition = {
+    calories: 1800,
+    carbs: 300,
+    lipids: 60,
+    protein: 60
+  }
+
+
+  ngOnInit() {
+
+  }
+
+  displayedColumns2: string[] = ["name",'calories_for100g', 'carbs', 'lipids', 'protein', 'type','action'];
+  dataSourceMenus :any= []
+  constructor(
+    private menuService: MenuServiceService,
+    private dialog: MatDialog,
+    private cdr: ChangeDetectorRef
+  ){
+  }
+
+
+
+  test(idList: any[]) {
+    for (let i = 0; i < idList.length; i++) {
+      console.log(idList[i].id)
+      this.menuService.getMenuWithFood(idList[i].id).subscribe(
+
+        data => {
+          let dataParse = JSON.parse(JSON.stringify(data));
+          console.log("size" + dataParse[0])
+          if (dataParse[0] == null) {
+            console.log("No data");
+            this.dataSourceMenus.push({id: idList[i].id, name: idList[i].name, foods: []});
+          }
+          else {
+            let totalNutrition =
+              {
+                calories: 0,
+                carbs: 0,
+                lipids: 0,
+                protein: 0
+              }
+            let dataParse = JSON.parse(JSON.stringify(data));
+            console.log(dataParse)
+            let foodList = [];
+            let menuItem:any= {id: dataParse[0].menu.id ,name : dataParse[0].menu.name, foods: []}
+
+            for (let i = 0; i < dataParse.length; i++) {
+              let quantity = dataParse[0].quantity;
+              totalNutrition.calories += ((dataParse[i].food.caloriesFor100g  )/100*quantity);
+              totalNutrition.carbs += ((dataParse[i].food.carbs)/100*quantity);
+              totalNutrition.lipids += ((dataParse[i].food.lipids)/100*quantity);
+              totalNutrition.protein += ((dataParse[i].food.protein)/100*quantity);
+              let item :any =
+                //aproxiamte 5.555 to 5.5
+                {
+                  relationId : dataParse[i].id,
+                  name: dataParse[i].food.name,
+                  calories: ((dataParse[i].food.caloriesFor100g  )/100*quantity).toFixed(1),
+                  carbs: ((dataParse[i].food.carbs)/100*quantity).toFixed(1),
+                  lipids: ((dataParse[i].food.lipids)/100*quantity).toFixed(1),
+                  protein: ((dataParse[i].food.protein)/100*quantity).toFixed(1),
+                  type: dataParse[i].food.type,
+                  quantity: quantity
+                }
+              foodList.push(item);
+            }
+            this.nutrition.calories -= totalNutrition.calories;
+            this.nutrition.carbs -= totalNutrition.carbs;
+            this.nutrition.lipids -= totalNutrition.lipids;
+            this.nutrition.protein -= totalNutrition.protein;
+
+            console.log(totalNutrition)
+            menuItem.foods =foodList;
+            menuItem.totalNutrition = totalNutrition;
+
+            this.dataSourceMenus.push(menuItem);
+
+          }
+
+        }
+      );
+    }
+
+  }
+  round (nombre: number, decimales: number): number {
+    const arrondi = Math.pow(10, decimales);
+    return Math.round(nombre * arrondi) / arrondi;
+  }
+
+  importMenu() {
+    const dialogRef = this.dialog.open(menuChoiceComponent, {
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result == null) {
+        return;
+      }
+      this.menuIdList.push(result);
+      this.dataSourceMenus = [];
+      this.test(this.menuIdList);
+      this.cdr.detectChanges();
+    });
+  }
+  newMenu() {
+
+  }
+
+
+  addFood(id: any) {
+    //open dialog
+    //add food to menu
+    const dialogRef = this.dialog.open(AddFoodComponent, {
+      data: {
+        menuId: id
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed', result);
+
+      result.menu.id = id;
+      console.log("menu : "+result.menu.id)
+      console.log("food : "+result.food.id)
+      this.menuService.addFoodToMenu(result).subscribe(
+        data => {
+          console.log(data);
+        }
+      )
+      setTimeout(() => {
+        this.cdr.detectChanges();
+        this.dataSourceMenus = [];
+        this.test(this.menuIdList)
+      }, 500);
+
+    });
+  }
+
+  deleteFood(relation: any) {
+    this.menuService.deleteMenuFoodWithId(relation).subscribe(
+      data => {
+        console.log(data);
+      }
+    )
+
+    setTimeout(() => {
+      this.cdr.detectChanges();
+      this.dataSourceMenus = [];
+      this.test(this.menuIdList)
+    }, 500);
+
+  }
+
+  addMenu() {
+    let idMenu:any;
+    if (this.menuNameInput.nativeElement.value != "") {
+    let data =
+      {
+        "name":this.menuNameInput.nativeElement.value
+      }
+    this.menuService.addMenu(data).subscribe(
+      data => {
+        this.menuIdList.push({id: data.id, name: this.menuNameInput.nativeElement.value});
+      }
+    )
+      setTimeout(() => {
+
+        console.log(this.menuIdList)
+        this.cdr.detectChanges();
+        this.dataSourceMenus = [];
+        this.test(this.menuIdList)
+      }, 100);
+    }
+  }
+}
---
+++ src/app/services/menu-service.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuServiceService {
+
+  constructor(private httpClient : HttpClient) { }
+
+  //get request as observable
+  getMenu() : Observable<Object> {
+    return this.httpClient.get('http://localhost:8080/api/v1/demo-controller');
+  }
+
+  getMenuWithFood(menuId : number) : Observable<any> {
+    return this.httpClient.get('http://localhost:8080/api/v1/menu_food/menu/' + menuId);
+  }
+
+  deleteMenuFoodWithId(relationId : number) : Observable<any> {
+    return this.httpClient.delete('http://localhost:8080/api/v1/menu_food/menu/' + relationId);
+  }
+
+  getMenuWithNameStartsWith(name : string) : Observable<any> {
+    return this.httpClient.get('http://localhost:8080/api/v1/menu/startingWith?startingChar=' + name);
+  }
+
+  addFoodToMenu(data :any) : Observable<any> {
+    return this.httpClient.post('http://localhost:8080/api/v1/menu_food', data);
+  }
+
+  addMenu(data :any) : Observable<any> {
+    return this.httpClient.post('http://localhost:8080/api/v1/menu', data);
+  }
+
+
+}
